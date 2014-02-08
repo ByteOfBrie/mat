294000fbddc44a47178f8cbbbb36293e960fa2eb
electromatter <electromatter@gmail.com>
2014-02-08 10:10:59 -0500

    Update compat.hpp

--- include/compat.hpp
+++ include/compat.hpp
@@ -2,19 +2,6 @@
 #define COMPAT_H
 
 #include <config.hpp>
-
-#ifndef MAT_IMPL_NO_STL
-# include <cstddef>
-# include <cstdio>
-# include <algorithm>
-# include <limits>
-# ifndef MAT_IMPL_NO_MATH
-#  include <cmath>
-# endif
-#else
-# include <stddef.h>
-#endif
-
 #include <math.hpp>
 
 #endif
